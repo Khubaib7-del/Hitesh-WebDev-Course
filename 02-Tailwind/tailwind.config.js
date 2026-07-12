f8abca47308efd64e978df27454a0deafb4badf8
Khubaib7-del <khubaibnazeer8@gmail.com>
2026-07-12 14:39:58 +0500

    feat: add Section 2 - Tailwind CSS styling code

---
+++ 02-Tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./cdnway/**/*.{html,js,jsx,ts,tsx}",
+    "./my-project/**/*.{html,js,jsx,ts,tsx}",
+    "./tailnext/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class', // Use 'class' for CSS class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
