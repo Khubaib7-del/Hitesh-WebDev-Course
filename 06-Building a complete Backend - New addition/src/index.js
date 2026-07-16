3cbe09587f3abb881aea1a9440ff50543b1065ca
Khubaib7-del <khubaibnazeer8@gmail.com>
2026-07-16 14:51:20 +0500

    feat: initialize MongoDB connection on server start

--- 06-Building a complete Backend - New addition/src/index.js
+++ 06-Building a complete Backend - New addition/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from './app.js';
+import connectDB from "./db/index.js";
 // you can access any variable from .env file using process.env.VARIABLE_NAME
 // configuring dotenv
 dotenv.config({
@@ -8,7 +9,13 @@ dotenv.config({
 
 const port = process.env.PORT || 3000;
 
+connectDB()
+    .then(() => {
 
-app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
+        app.listen(port, () => {
+            console.log(`Example app listening on port http://localhost:${port}`)
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error",err)
+    })
